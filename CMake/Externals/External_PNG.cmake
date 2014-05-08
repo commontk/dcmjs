6ecce1aae00b51a95f1c3ab51ffccc71c6acf6f2
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2014-05-08 13:05:28 -0400

    Enable PNG support in DCMTK
    Also generalize who Emscripten compiler and flags are passed to the
    different projects.

---
+++ CMake/Externals/External_PNG.cmake
@@ -0,0 +1,61 @@
+
+set(proj PNG)
+
+# Set dependency list
+set(${proj}_DEPENDENCIES ZLIB)
+
+# Include dependent projects if any
+ExternalProject_Include_Dependencies(${proj} PROJECT_VAR proj DEPENDS_VAR ${proj}_DEPENDENCIES)
+
+if(${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj})
+  message(FATAL_ERROR "Enabling ${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj} is not supported !")
+endif()
+
+# Sanity checks
+if(DEFINED ${proj}_DIR AND NOT EXISTS ${${proj}_DIR})
+  message(FATAL_ERROR "${proj}_DIR variable is defined but corresponds to non-existing directory")
+endif()
+
+if(NOT DEFINED ${proj}_DIR AND NOT ${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj})
+
+  set(EP_SOURCE_DIR ${CMAKE_BINARY_DIR}/${proj})
+  set(EP_BINARY_DIR ${CMAKE_BINARY_DIR}/${proj}-build)
+  set(EP_INSTALL_DIR ${CMAKE_BINARY_DIR}/${proj}-install)
+
+  ExternalProject_Add(${proj}
+    ${${proj}_EP_ARGS}
+    URL "http://download.sourceforge.net/libpng/libpng-1.6.10.tar.gz"
+    URL_MD5 "b0f0c38a02dc92f70f0053a40abc7e0b"
+    SOURCE_DIR ${EP_SOURCE_DIR}
+    BINARY_DIR ${EP_BINARY_DIR}
+    INSTALL_DIR ${EP_INSTALL_DIR}
+    CMAKE_CACHE_ARGS
+      ${EMSCRIPTEN_TOOLCHAIN_OPTIONS}
+      # PNG options
+      -DPNG_TESTS:BOOL=OFF
+      -DPNG_SHARED:BOOL=ON
+      -DPNG_STATIC:BOOL=OFF
+      -DM_LIBRARY:FILEPATH= # Disable floating point
+      -DCMAKE_INSTALL_PREFIX:PATH=<INSTALL_DIR>
+    DEPENDS
+      ${${proj}_DEPENDENCIES}
+    )
+  set(${proj}_DIR ${EP_INSTALL_DIR})
+  set(${proj}_INCLUDE_DIR ${${proj}_DIR}/include)
+  set(${proj}_LIBRARY ${${proj}_DIR}/lib/libpng16.so)
+else()
+  # The project is provided using <proj>_DIR, nevertheless since other project may depend on <proj>,
+  # let's add an 'empty' one
+  ExternalProject_Add_Empty(${proj} DEPENDS ${${proj}_DEPENDENCIES})
+endif()
+
+mark_as_superbuild(
+  VARS
+    ${proj}_INCLUDE_DIR:PATH
+    ${proj}_LIBRARY:FILEPATH
+  PROJECTS DCMTK
+  LABELS "FIND_PACKAGE"
+  )
+
+ExternalProject_Message(${proj} "${proj}_INCLUDE_DIR:${${proj}_INCLUDE_DIR}")
+ExternalProject_Message(${proj} "${proj}_LIBRARY:${${proj}_LIBRARY}")
---
+++ CMake/Externals/External_ZLIB.cmake
@@ -0,0 +1,67 @@
+
+set(proj ZLIB)
+
+# Set dependency list
+set(${proj}_DEPENDENCIES "")
+
+# Include dependent projects if any
+ExternalProject_Include_Dependencies(${proj} PROJECT_VAR proj DEPENDS_VAR ${proj}_DEPENDENCIES)
+
+if(${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj})
+  
+endif()
+
+# Sanity checks
+if(DEFINED ${proj}_DIR AND NOT EXISTS ${${proj}_DIR})
+  message(FATAL_ERROR "<proj>_DIR variable is defined but corresponds to non-existing directory")
+endif()
+
+if(NOT DEFINED ${proj}_DIR AND NOT ${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj})
+
+  if(NOT DEFINED git_protocol)
+    set(git_protocol "git")
+  endif()
+
+  set(EP_SOURCE_DIR ${CMAKE_BINARY_DIR}/${proj})
+  set(EP_BINARY_DIR ${CMAKE_BINARY_DIR}/${proj}-build)
+  set(EP_INSTALL_DIR ${CMAKE_BINARY_DIR}/${proj}-install)
+
+  ExternalProject_Add(${proj}
+    ${${proj}_EP_ARGS}
+    GIT_REPOSITORY "${git_protocol}://github.com/commontk/zlib.git"
+    GIT_TAG "66a753054b356da85e1838a081aa94287226823e"
+    SOURCE_DIR ${EP_SOURCE_DIR}
+    BINARY_DIR ${EP_BINARY_DIR}
+    INSTALL_DIR ${EP_INSTALL_DIR}
+    CMAKE_CACHE_ARGS
+      ${EMSCRIPTEN_TOOLCHAIN_OPTIONS}
+      -DBUILD_SHARED_LIBS:BOOL=ON
+      -DZLIB_MANGLE_PREFIX:STRING=dcmjs_zlib_
+      -DCMAKE_INSTALL_PREFIX:PATH=<INSTALL_DIR>
+    DEPENDS
+      ${${proj}_DEPENDENCIES}
+    )
+  set(${proj}_DIR ${EP_INSTALL_DIR})
+  set(${proj}_ROOT ${${proj}_DIR})
+  set(${proj}_INCLUDE_DIR ${${proj}_DIR}/include)
+  set(${proj}_LIBRARY ${${proj}_DIR}/lib/libzlib.so)
+else()
+  # The project is provided using <proj>_DIR, nevertheless since other project may depend on <proj>,
+  # let's add an 'empty' one
+  ExternalProject_Add_Empty(${proj} DEPENDS ${${proj}_DEPENDENCIES})
+endif()
+
+mark_as_superbuild(
+  VARS
+    ${proj}_INCLUDE_DIR:PATH
+    ${proj}_LIBRARY:FILEPATH
+    ${proj}_ROOT:PATH
+  PROJECTS DCMTK PNG
+  LABELS "FIND_PACKAGE"
+  )
+
+ExternalProject_Message(${proj} "${proj}_INCLUDE_DIR:${${proj}_INCLUDE_DIR}")
+ExternalProject_Message(${proj} "${proj}_LIBRARY:${${proj}_LIBRARY}")
+if(${proj}_ROOT)
+  ExternalProject_Message(${proj} "${proj}_ROOT:${${proj}_ROOT}")
+endif()
