6478d4317af51961de7c17f91acbd4e67d43c107
Steve Pieper <pieper@isomics.com>
2014-05-08 10:45:43 -0400

    Add commas to json object definition (runtime syntax error)

--- javascripts/pre.js
+++ javascripts/pre.js
@@ -1,6 +1,6 @@
 
 // See https://github.com/kripken/emscripten/wiki/Interacting-with-code
 var Module = {
-  'noInitialRun' : 1
-  'noExitRuntime' : 1
+  'noInitialRun' : 1,
+  'noExitRuntime' : 1,
 };
