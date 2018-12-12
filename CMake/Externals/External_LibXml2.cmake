be0e6cda4d7b3d0bf98501d79f3aaebcfa60f989
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2018-12-12 13:10:12 -0500

    cmake: CMake'ified LibXml2 to support building shared lib
    This is needed to avoid duplicated symbol issue when enabling xml2dsr
    application. See #23
    
    Indeed, trying to configure with "--disable-static --enable-shared" was
    failing and attempting to patch the build system was not straightforward.
    
    This commit adds a simply CMakeLists.txt along with pre-configured headers
    config.h and xmlversion.h suitable for emscripten. These headers have first
    been obtained by configuring the project with:
    
     emconfigure ./configure  \
      --without-python \
      --without-iconv \
      --without-http \
      --without-ftp \
      --disable-shared --enable-static

--- CMake/Externals/External_LibXml2.cmake
+++ CMake/Externals/External_LibXml2.cmake
@@ -8,55 +8,22 @@ set(${proj}_DEPENDENCIES "")
 ExternalProject_Include_Dependencies(${proj} PROJECT_VAR proj DEPENDS_VAR ${proj}_DEPENDENCIES)
 
 if(${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj})
-  
+  message(FATAL_ERROR "Enabling ${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj} is not supported !")
 endif()
 
 # Sanity checks
-if(DEFINED ${proj}_DIR AND NOT EXISTS ${${proj}_DIR})
-  message(FATAL_ERROR "<proj>_DIR variable is defined but corresponds to non-existing directory")
+if(DEFINED LIBXML2_INCLUDE_DIR AND NOT EXISTS ${LIBXML2_INCLUDE_DIR})
+  message(FATAL_ERROR "LIBXML2_INCLUDE_DIR variable is defined but corresponds to non-existing directory")
+endif()
+if(DEFINED LIBXML2_LIBRARIES AND NOT EXISTS ${LIBXML2_LIBRARIES})
+  message(FATAL_ERROR "LIBXML2_LIBRARIES variable is defined but corresponds to non-existing file")
 endif()
 
-if(NOT DEFINED ${proj}_DIR AND NOT ${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj})
+if((NOT DEFINED LIBXML2_INCLUDE_DIR OR NOT DEFINED LIBXML2_LIBRARIES) AND NOT ${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj})
 
   set(EP_SOURCE_DIR ${CMAKE_BINARY_DIR}/${proj})
   set(EP_BINARY_DIR ${CMAKE_BINARY_DIR}/${proj}-build)
-  set(EP_INSTALL_DIR ${CMAKE_BINARY_DIR}/${proj}-install)
-  
-  include(ExternalProjectForNonCMakeProject)
 
-  # Adapted from http://mozakai.blogspot.com/2012/03/howto-port-cc-library-to-javascript.html
-
-  # environment
-  set(_env_script ${CMAKE_BINARY_DIR}/${proj}_Env.cmake)
-  ExternalProject_Write_SetBuildEnv_Commands(${_env_script})
-  file(APPEND ${_env_script}
-"#------------------------------------------------------------------------------
-# Added by '${CMAKE_CURRENT_LIST_FILE}'
-include(\"${${CMAKE_PROJECT_NAME}_CMAKE_DIR}/ExternalProjectForNonCMakeProject.cmake\")
-set(CMAKE_BINARY_DIR \"${CMAKE_BINARY_DIR}\")
-
-")
-
-  set(_src_dir ${EP_SOURCE_DIR})
-
-  # configure step
-  set(_configure_script ${CMAKE_BINARY_DIR}/${proj}_configure_step.cmake)
-  file(WRITE ${_configure_script}
-"include(\"${_env_script}\")
-set(${proj}_WORKING_DIR \"${_src_dir}\")
-ExternalProject_Execute(${proj} \"configure\" ${EMSCRIPTEN_ROOT_PATH}/emconfigure ./configure
-  --without-python --without-iconv --without-http --without-ftp
-  ) 
-")
-
-  # build step
-  set(_build_script ${CMAKE_BINARY_DIR}/${proj}_build_step.cmake)
-  file(WRITE ${_build_script}
-"include(\"${_env_script}\")
-set(${proj}_WORKING_DIR \"${_src_dir}\")
-ExternalProject_Execute(${proj} \"build\" ${EMSCRIPTEN_ROOT_PATH}/emmake make)
-")
-    
   ExternalProject_Add(${proj}
     ${${proj}_EP_ARGS}
     URL "ftp://xmlsoft.org/libxml2/libxml2-2.9.8.tar.gz"
@@ -64,22 +31,33 @@ ExternalProject_Execute(${proj} \"build\" ${EMSCRIPTEN_ROOT_PATH}/emmake make)
     DOWNLOAD_DIR ${DOWNLOAD_CACHE_DIR}
     DOWNLOAD_NO_PROGRESS 1
     SOURCE_DIR ${EP_SOURCE_DIR}
-    BUILD_IN_SOURCE 1
-    CONFIGURE_COMMAND ${CMAKE_COMMAND} -P ${_configure_script}
-    BUILD_COMMAND ${CMAKE_COMMAND} -P ${_build_script}
+    BINARY_DIR ${EP_BINARY_DIR}
+    PATCH_COMMAND
+      ${CMAKE_COMMAND} -E copy
+        ${CMAKE_SOURCE_DIR}/CMake/Externals/LibXml2-CMakeLists.txt
+        ${EP_SOURCE_DIR}/CMakeLists.txt
+      COMMAND ${CMAKE_COMMAND} -E copy
+        ${CMAKE_SOURCE_DIR}/CMake/Externals/LibXml2-config.h
+        ${EP_SOURCE_DIR}/include/config.h
+      COMMAND ${CMAKE_COMMAND} -E copy
+        ${CMAKE_SOURCE_DIR}/CMake/Externals/LibXml2-xmlversion.h
+        ${EP_SOURCE_DIR}/include/libxml/xmlversion.h
+    CMAKE_CACHE_ARGS
+      ${EMSCRIPTEN_TOOLCHAIN_OPTIONS}
+      # LibXml2 options
+    INSTALL_COMMAND ""
     USES_TERMINAL_DOWNLOAD 1
     USES_TERMINAL_UPDATE 1
     USES_TERMINAL_CONFIGURE 1
     USES_TERMINAL_BUILD 1
-    INSTALL_COMMAND ""
+    USES_TERMINAL_INSTALL 1
     DEPENDS
       ${${proj}_DEPENDENCIES}
     )
-  set(${proj}_DIR ${_src_dir})
-  set(LIBXML2_INCLUDE_DIR ${${proj}_DIR}/include)
-  set(LIBXML2_LIBRARIES ${${proj}_DIR}/.libs/libxml2.a)
+  set(LIBXML2_INCLUDE_DIR ${EP_SOURCE_DIR}/include)
+  set(LIBXML2_LIBRARIES ${EP_BINARY_DIR}/libxml2.so)
 else()
-  # The project is provided using <proj>_DIR, nevertheless since other project may depend on <proj>,
+  # The project is provided using LIBXML2_LIBRARIES and LIBXML2_LIBRARIES, nevertheless since other project may depend on <proj>,
   # let's add an 'empty' one
   ExternalProject_Add_Empty(${proj} DEPENDS ${${proj}_DEPENDENCIES})
 endif()
