d56d56829010a3a30a418a8d40726544962d32aa
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2019-06-03 02:02:07 -0400

    Clean-up pre.js removing unneeded setting of Module and noExitRuntime
    * the Module variable is already set in module before the transclusion of pre.js
    * noExitRuntime is implied. See https://emscripten.org/docs/getting_started/FAQ.html?highlight=exit_runtime#what-does-exiting-the-runtime-mean-why-don-t-atexit-s-run

--- javascripts/pre.js
+++ javascripts/pre.js
@@ -1,11 +1,8 @@
 
 var ENVIRONMENT_IS_WEB = typeof window === 'object';
 
-var Module = Module || {};
-
 // See https://github.com/kripken/emscripten/wiki/Interacting-with-code
 if (ENVIRONMENT_IS_WEB)
   {
   Module['noInitialRun'] = 1;
-  Module['noExitRuntime'] = 1;
   }
