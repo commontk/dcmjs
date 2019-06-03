d52aa5874606b6e847ed82f37ab3edf0a73f7201
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2019-06-03 02:46:35 -0400

    Support use of module using "require()". See #27

--- javascripts/pre.js
+++ javascripts/pre.js
@@ -1,8 +1,1 @@
-
-var ENVIRONMENT_IS_WEB = typeof window === 'object';
-
-// See https://github.com/kripken/emscripten/wiki/Interacting-with-code
-if (ENVIRONMENT_IS_WEB)
-  {
-  Module['noInitialRun'] = 1;
-  }
+Module['noInitialRun'] = 1;
